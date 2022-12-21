c8894d0fe32ca8a1c47b76a22e0e00c454cb146a
Kamil <kamilrogozinski29@gmail.com>
2022-12-21 14:48:34 +0000

    Created the first function to generate computer answer

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 /* Computer chooses randomly from Rock,Paper Scissors */
-let computerChoice;
 function getComputerChoice() {
-   
+    let answer = Math.floor(Math.random()* 3) + 1;
+    if(answer == 1) {
+        answer = "Rock";
+    } else if(answer == 2) {
+        answer = "Paper";
+    } else {
+        answer = "Scissors"
+    }
+    console.log(answer);
 }
+
+getComputerChoice();
