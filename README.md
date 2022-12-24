7602c3b0a131e81f6a3e7541baeb8ced7a5b867b
Kamil <kamilrogozinski29@gmail.com>
2022-12-24 13:19:35 +0000

    Wroking on game logic

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Computer chooses randomly from Rock,Paper or Scissors
     -The answer has to be random from the three option 
     -Create a variable to store the answer of this function
     -Randomly create a number from 1-3 and assign a value for 1-3 for R,P,S
-    
 You choose from Rock,Paper or Scissors
 You set the rules/logic of the game
 Play your answer against the computer and store the answer in a variable
--- script.js
+++ script.js
@@ -6,9 +6,40 @@ function getComputerChoice() {
     } else if(answer == 2) {
         answer = "Paper";
     } else {
-        answer = "Scissors"
+        answer = "Scissors";
     }
-    console.log(answer);
+    return answer;
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+// I thought that my changing the word to a number I can compare the answer better
+    // if(playerSelection == "Rock") {
+    //     playerSelection = 1;
+    // } else if(playerSelection == "Paper") {
+    //     playerSelection = 2;
+    // } else if(playerSelection = "Scissors") {
+    //     playerSelection = 3;
+    // }
+
+    // if(computerSelection == "Rock") {
+    //     computerSelection = 1;
+    // } else if(computerSelection == "Paper") {
+    //     computerSelection = 2;
+    // } else if(computerSelection = "Scissors") {
+    //     computerSelection = 3;
+    // }
+    // debugger;
+
+   if(playerSelection === computerSelection) {
+    return "Tie";
+   } else if(playerSelection > computerSelection) {
+    return "You Win";
+   } else if(playerSelection < computerSelection) {
+    return "You Lost";
+   }
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = "Scissors";
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
